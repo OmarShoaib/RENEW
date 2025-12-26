53183d0654e205b481184461d83c464a0bb8b994
alimuhammad1999 <alianwar1999@gmail.com>
2025-12-26 16:12:02 +0500

    skips

--- app/src/main/java/edu/aku/omarshoaib/renew/activity/sections/SectionF05.java
+++ app/src/main/java/edu/aku/omarshoaib/renew/activity/sections/SectionF05.java
@@ -97,7 +97,7 @@ public class SectionF05 extends BaseActivity {
 
         if(!proceed()) {
             AppConstants.showSimpleSnackBar(activity,
-                    "Child noe eligible for enrollment", AppConstants.TYPE_ERROR);
+                    "Child not eligible for enrollment", AppConstants.TYPE_ERROR);
             return false;
         }
 
@@ -116,7 +116,7 @@ public class SectionF05 extends BaseActivity {
     private boolean proceed() {
         boolean lowMuac = !sF5.getF512().isEmpty() && Float.parseFloat(sF5.getF512()) < 12.5f;
         boolean edema = sF5.getFo515().equals("1");
-        return !(lowMuac || edema);
+        return lowMuac || edema;
     }
 
     public void btnContinue(View view) {
